883d265cfa742cbaf53194d6238157c43e21cb26
Navya <93501878+gitnavya0@users.noreply.github.com>
2023-10-26 15:21:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,50 @@
 const express = require('express');
 const connection = require('./database');
-const bodyParser = require("body-parser");
-const encoder = bodyParser.urlencoded();
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.use("/assets", express.static("assets"));
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/auth.html");
+    res.sendFile(__dirname + '/auth.html');
 });
 
-app.post("/", encoder, function (req, res) {
+app.post('/', function (req, res) {
     const role = req.body.selectedRole;
-    var username = req.body.username;
-    var password = req.body.password;
     if (role == 'Manager') {
-        connection.query(`select * from manager_login_details where username=? and password=?`, [username, password], function (error, results, fields) {
+        var username = req.body.username;
+        var password = req.body.password;
+        connection.query('select * from manager_login_details where username=? and password=?', [username, password], function (error, results, fields) {
+            if (error) {
+                console.error('Error querying the database: ' + error);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
             if (results.length > 0) {
-                res.redirect("/home");
+                res.redirect('/home');
+            } else {
+                res.send('<script>alert("Incorrect username or password, Please try again!"); window.location = "/";</script>');
             }
-            else {
-                res.redirect("/");
-            }
-        })
-    }
-    else if (role == 'Customer') {
+        });
+    } else if (role == 'Customer') {
         // Handle customer authentication here.
     }
 });
 
-app.get("/home", function (req, res) {
-    res.sendFile(__dirname + "/home.html");
+app.get('/home', function (req, res) {
+    connection.query('SELECT COUNT(*) AS branchCount FROM branch', function (error, results, fields) {
+        if (error) {
+            console.error('Error querying the database: ' + error);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        const branchCount = results[0].branchCount;
+        res.render('home', { branchCount });
+    });
 });
 
 const PORT = 3000;
